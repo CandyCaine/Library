37564aa7ed5ba45faa2da071dd65204d0144c572
CandyCaine <candycainenet@gmail.com>
2012-09-24 13:52:01 +0100

    Added getDesktopHints() method.  Can be used for anti-aliasing text.  Added setAlpha() method.  Changes alpha value of a BufferedImage.

--- src/me/rich/lib/graphics/ImageUtil.java
+++ src/me/rich/lib/graphics/ImageUtil.java
@@ -8,9 +8,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -70,6 +72,30 @@ public class ImageUtil {
 		g.rotate(Math.toRadians(angle), width / 2, height / 2);
 		g.drawImage(src, null, 0, 0);
 		g.dispose();
+		return this.toCompatibleImage(image);
+	}
+	
+	public Map<?, ?> getDesktopHints() {
+		return (Map<?, ?>) (Toolkit.getDefaultToolkit().getDesktopProperty("awt.font.desktophints"));
+	}
+	
+	public BufferedImage setAlpha(BufferedImage src, int opacity) {
+		BufferedImage image = this.toCompatibleImage(src);
+		
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				int pixel = image.getRGB(x, y);
+				int alpha = (pixel & 0x0FF000000) >> 24;
+				int red = (pixel & 0x00FF0000) >> 16;
+				int green = (pixel & 0x0000FF00) >> 8;
+				int blue = (pixel & 0x000000FF);
+				
+				alpha = opacity;
+				
+				int newRGB = ((alpha & 0x0FF) << 24 | (red & 0x0FF) << 16 | (green & 0x0FF) << 8 | (blue & 0x0FF));
+				image.setRGB(x, y, newRGB);
+			}
+		}
 		return this.toCompatibleImage(image);
 	}
 }
